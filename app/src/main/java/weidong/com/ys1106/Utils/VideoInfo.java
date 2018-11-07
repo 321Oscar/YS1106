211c17631331594595daa11cbbe8162a08a23efd
WeiDong <975867938@qq.com>
2018-11-07 17:36:41 +0800

    add adapter and videoinfo

---
+++ app/src/main/java/weidong/com/ys1106/Utils/VideoInfo.java
@@ -0,0 +1,57 @@
+package weidong.com.ys1106.Utils;
+
+public class VideoInfo {
+    public String ImgUrl;
+    public String videotitle,videodetail;
+    public String VideoType;
+    public int checked;
+
+    public VideoInfo(){
+
+    }
+
+    public VideoInfo(String imgUrl,String videodetail,String videotitle,int checked){
+        this.checked = checked;
+        this.ImgUrl = imgUrl;
+        this.videodetail = videodetail;
+        this.videotitle = videotitle;
+    }
+
+    public int getChecked() {
+        return checked;
+    }
+
+    public void setChecked(int checked) {
+        this.checked = checked;
+    }
+
+    public String getImgUrl() {
+        return ImgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        ImgUrl = imgUrl;
+    }
+
+    public String getVideodetail() {
+        return videodetail;
+    }
+
+    public void setVideodetail(String videodetail) {
+        this.videodetail = videodetail;
+    }
+
+    public String getVideotitle() {
+        return videotitle;
+    }
+
+    public void setVideotitle(String videotitle) {
+        this.videotitle = videotitle;
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
